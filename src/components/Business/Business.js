35bc7c4594394ccbc65612a6da35d2701f6df4e4
Shabab <shababsaifi@gmail.com>
2020-09-04 20:37:14 +0530

    created BusinessList component

--- src/components/Business/Business.js
+++ src/components/Business/Business.js
@@ -14,7 +14,7 @@ const business = {
   reviewCount: 90
 }
 
-export class Business extends React.Component {
+class Business extends React.Component {
   render() {
     return (
       <div className="Business">
@@ -38,3 +38,5 @@ export class Business extends React.Component {
     )
   }
 }
+
+export default Business;
