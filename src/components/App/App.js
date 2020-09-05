468fc4b340f9f928b8504eef3587e8a4d082183c
Shabab <shababsaifi@gmail.com>
2020-09-05 12:41:55 +0530

    Moved business data to App.js

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,13 +3,26 @@ import BusinessList from '../BusinessList/BusinessList';
 import SearchBar from '../SearchBar/SearchBar';
 import './App.css';
 
+const business = {
+  imageSrc: 'https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg',
+  name: 'MarginOtto Pizzeria',
+  address: '1010 Paddington Way',
+  city: 'Flavortown',
+  state: 'NY',
+  zipCode: '10101',
+  category: 'Italian',
+  rating: 4.5,
+  reviewCount: 90,
+  businesses: []
+}
+
 class App extends React.Component {
   render() {
     return (
       <div className="App">
         <h1>ravenous</h1>
         <SearchBar />
-        <BusinessList />
+        <BusinessList businesses={business.businesses}/>
       </div>
     ); 
   }
